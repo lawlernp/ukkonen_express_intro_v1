9a5fad7f1ef307ad297c1b8f77466103ec7db05f
Casie Siekman <casie.siekman@gmail.com>
2020-10-06 17:01:32 -0500

    add server code

--- server/server.js
+++ server/server.js
@@ -6,8 +6,27 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const quotesData = require ('./modules/quotes.js');
+
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
+
+
+let index = 0;
+
+app.get('/quotes', (req, res) => {
+    console.log('hi from get request');
+    res.send(quotesData);
+})
+
+app.get('/randomQuote', (req, res) => {
+    let randomNumber = getRandomInt(quotesData.length);
+    res.send(quotesData[randomNumber]);
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
 app.listen(port, () => {
     console.log("Up and running on port: ", port);
