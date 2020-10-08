2fd03717bcedce8affe6c0732900790b8bb8a4bb
lawlernp <lawlernp@gmail.com>
2020-10-08 09:40:47 -0500

    added input boxes and linked to js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,29 @@ $(document).ready(onReady)
 function onReady(){
     console.log('hi from jq');
     getRandomQuote();
+    $('#submit').on('click', submitQuote);
 };
+
+function submitQuote() {
+    let quote = $('#quote').val();
+    let author = $('#author').val();
+    console.log(quote, author);
+    $.ajax({
+        method: 'post',
+        url: '/quotes',
+        data:{
+            quote: quote,
+            author: author
+        }
+    }).then(function(response){
+        console.log('response', response);
+        
+    }).catch(function(error){
+        alert(error);
+    });
+};
+
+
 function getRandomQuote() {
     console.log('get the quote');
     $.ajax({
@@ -17,7 +39,7 @@ function getRandomQuote() {
 
 function appendToDom(dataToAppend) {
     $('#output').append(`
-        ${dataToAppend.quote}</br><i>from: ${dataToAppend.author}</i>
+        <p>${dataToAppend.quote}</p><i>from: ${dataToAppend.author}</i>
     `)
 }
 console.log("hello from JS");
